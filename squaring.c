6f66e75699f1b39d3baf1fc9759151c2f6bd0c66
Henry <arboladora.henry@gmail.com>
2024-11-07 18:43:44 -0500

    Init commit for squaring

--- squaring.c
+++ squaring.c
@@ -3,15 +3,24 @@
  * file: squaring.c
  * authors: Timmy Huy Xuan Ngo, Henry Arboladora
  * co-authors: Christian Sorto, Fairuj Tabassum 
- * description: program to TODO
+ * description: program to square the current sample of the derivative signal to emphasize higher frequencies.
  */
 
 // Link
 #include <stdio.h>
+#define BUFFSIZE 600    // The size of the buffers (in samples). Must fit more than 1.66 times an RR interval, 
+                        // which typically could be around 1 second.
 
 // Definition
+// Aims to remove negative frequencies and emphasize higher frequencies.
+// The equation is as follows: y(nT) = [x(nT)]^2. 
+// Where x(nT) is the output of the derivative filter for the current sample.
 
 // Global Declaration
+int squared[BUFFSIZE];
 
 // Function
-// TODO@HuySpring883
+void squareFunc(int current, int derivative) {
+    // Square the current derivative value and store it in the squared array at the current index
+    squared[current] = derivative * derivative;
+}
---
+++ squaring.h
@@ -0,0 +1,10 @@
+// squared.h
+#ifndef SQUARED_H
+#define SQUARED_H
+
+#define BUFFSIZE 600
+extern int squared[BUFFSIZE];  // Declare squared as an external variable
+
+void squareFunc(int length, int derivative);  // Function prototype
+
+#endif
